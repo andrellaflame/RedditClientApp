d405247cd042980a85b63531e72d5edf5b1ebd3e
andrellaflame <sulimenkoandrey04@gmail.com>
2023-03-02 14:40:41 -0500

    HT_3

--- Sulimenko_HT_2/HTTPRequest/NetworkService.swift
+++ Sulimenko_HT_2/HTTPRequest/NetworkService.swift
@@ -14,11 +14,9 @@ class NetworkService {
             headers.append($0.key + "=" + $0.value + "&")
         }
         let link = "https://www.reddit.com/r/\(subreddit)/top.json?\(headers)"
-        
         guard let url = URL(string: link) else { return }
         
         URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
-            
             guard let data = data, let redditData = try? JSONDecoder().decode(RedditData.self, from: data) else { return }
             PostListViewController.Const.afterParameter = redditData.data.after
             onComplete(redditData.data.children.map({ return PostData(from: $0.data) }))
